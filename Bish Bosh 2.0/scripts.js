990accdce18e8bbdb04fcf721d96357dea354cb3
Clive <Cliveswe@users.noreply.github.com>
2025-05-09 11:40:41 +0200

    Moved the script to a file.

---
+++ Bish Bosh 2.0/scripts.js
@@ -0,0 +1,20 @@
+ const output = document.getElementById('output');
+            let number = ``;
+
+            for (let i = 1; i <= 100; i++) {
+                if (i % 3 === 0 && i % 4 === 0) {
+                    number += `Bish-Bosh, `;
+                } else if (i % 3 === 0) {
+                    number += `Bish, `;
+                } else if (i % 4 === 0) {
+                    number += `Bosh, `;
+                } else {
+                    number += `${i}, `
+                }
+                if (i % 13 === 0) {
+                    number += `\n`;
+                }
+
+            }
+
+            output.textContent = number;
