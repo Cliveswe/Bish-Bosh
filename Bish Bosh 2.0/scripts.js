9716f31423fdd59cfa74310035a94339d8ec90bd
Clive <Cliveswe@users.noreply.github.com>
2025-05-09 13:31:37 +0200

    Completed the validation. Reset the form with JS. Added additional error messages and clearing previous error message if any.

--- Bish Bosh 2.0/scripts.js
+++ Bish Bosh 2.0/scripts.js
@@ -1,5 +1,6 @@
+const form = document.getElementById('bishBoshForm');
 
-document.getElementById('bishBoshForm').addEventListener('submit', function (event) {
+form.addEventListener('submit', function (event) {
     // Prevent the form from submitting.
     event.preventDefault();
 
@@ -7,14 +8,38 @@ document.getElementById('bishBoshForm').addEventListener('submit', function (eve
     const max = parseInt(document.getElementById('maxNumber').value, 10);
     const bish = parseInt(document.getElementById('bishDivisor').value, 10);
     const bosh = parseInt(document.getElementById('boshDivisor').value, 10);
+
     // Output target.
     const error = document.getElementById('error');
     const output = document.getElementById('output');
+
+    // Clear previous output.
+    output.textContent = ``;
+    //Clear any previous error messages.
+    error.textContent = ``;
+
     //Results 
     let number = ``;
-    let errorMessage= ``;
-//validation
 
+
+    //validation
+    if (isNaN(max) || max <= 0) {
+        error.textContent = `Max number must be a positive number and granter 1.\n`;
+        return;
+    }
+
+    if (isNaN(bish) || isNaN(bosh) ||
+        bish <= 0 || bosh <= 0 ||
+        bish >= max || bosh >= max) {
+        error.textContent = `Bish and Bosh must be positive numbers and greater than 1 but less than ${max}.\n`;
+        return;
+    }
+
+
+    if (bish == bosh) {
+        error.textContent = `Bish and Bosh must be different numbers.\n`;
+        return;
+    }
 
     for (let i = 1; i <= max; i++) {
         if (i % bish === 0 && i % bosh === 0) {
@@ -32,5 +57,10 @@ document.getElementById('bishBoshForm').addEventListener('submit', function (eve
 
     }
 
+   
+    // Display the results of Bish Bosh 2.0.
     output.textContent = number;
+
+    //Reset the form on success.
+    form.reset();
 });
