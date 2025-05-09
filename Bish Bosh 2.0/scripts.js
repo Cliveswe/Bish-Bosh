0a4009a8c75957f8009ea76c53d5e341481f0d26
Clive <Cliveswe@users.noreply.github.com>
2025-05-09 11:51:03 +0200

    Updated the index.html to  get the script.js file. Added an event listener to the form submit button.

--- Bish Bosh 2.0/scripts.js
+++ Bish Bosh 2.0/scripts.js
@@ -1,20 +1,30 @@
- const output = document.getElementById('output');
-            let number = ``;
 
-            for (let i = 1; i <= 100; i++) {
-                if (i % 3 === 0 && i % 4 === 0) {
-                    number += `Bish-Bosh, `;
-                } else if (i % 3 === 0) {
-                    number += `Bish, `;
-                } else if (i % 4 === 0) {
-                    number += `Bosh, `;
-                } else {
-                    number += `${i}, `
-                }
-                if (i % 13 === 0) {
-                    number += `\n`;
-                }
+document.getElementById('bishBoshForm').addEventListener('submit', function (event) {
+    // Prevent the form from submitting.
+    event.preventDefault();
+    // Get the values from the form.
+    const maxNumber = document.getElementById('maxNumber').value;
+    const bishNumber = document.getElementById('bishNumber').value;
+    const boshNumber = document.getElementById('boshNumbier').value;
 
-            }
+    const output = document.getElementById('output');
+    let number = ``;
 
-            output.textContent = number;
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 4 === 0) {
+            number += `Bish-Bosh, `;
+        } else if (i % 3 === 0) {
+            number += `Bish, `;
+        } else if (i % 4 === 0) {
+            number += `Bosh, `;
+        } else {
+            number += `${i}, `
+        }
+        if (i % 13 === 0) {
+            number += `\n`;
+        }
+
+    }
+
+    output.textContent = number;
+});
