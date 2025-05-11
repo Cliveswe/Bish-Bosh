4739c1d27058bc0db972d66a1d4d7dca73255c51
Clive <Cliveswe@users.noreply.github.com>
2025-05-11 11:21:36 +0200

    Determined the max and min width of the application.

--- ToDo List/script.js
+++ ToDo List/script.js
@@ -4,6 +4,8 @@ function addTodo() {
     const task = input.value.trim();
     const feedback = input.nextElementSibling.nextElementSibling;
 
+    //Validate the input.
+    // If the input is empty, add the 'is-invalid' class and show the feedback message.
     if (task === '') {
         input.classList.add('is-invalid');
         feedback.style.display = 'block';
